d9a6830672e44924db2581d6b925bbe7394a7fb9
Joonas Salmela <joonas.salmela@gmail.com>
2014-11-19 19:14:00 +0200

    Added build for multiplyScalar() and addScalar().

--- build/victor.js
+++ build/victor.js
@@ -419,6 +419,47 @@ Victor.prototype.multiply = function (vector) {
 };
 
 /**
+ * Multiplies both vector axis by the given scalar
+ *
+ * ### Examples:
+ *     var vec = new Victor(1, 2);
+ *
+ *     vec.multiplyScalar(2);
+ *     vec.toString();
+ *     // => x: 2, y: 4
+ *
+ * @param {Number} scalar The scalar to multiply by
+ * @return {Victor} `this` for chaining capabilities
+ * @api public
+ */
+Victor.prototype.multiplyScalar = function (scalar) {
+	this.x *= scalar;
+	this.y *= scalar;
+	return this;
+};
+
+
+/**
+ * Adds the given scalar to both vector axis
+ *
+ * ### Examples:
+ *     var vec = new Victor(1, 2);
+ *
+ *     vec.addScalar(2);
+ *     vec.toString();
+ *     // => x: 3, y: 4
+ *
+ * @param {Number} scalar The scalar to add
+ * @return {Victor} `this` for chaining capabilities
+ * @api public
+ */
+Victor.prototype.addScalar = function (scalar) {
+	this.x += scalar;
+	this.y += scalar;
+	return this;
+};
+
+/**
  * Normalize
  *
  * @return {Victor} `this` for chaining capabilities
