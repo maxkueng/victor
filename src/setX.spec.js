8cd04c66fff02abfb2f7ccd3bcd23760d5a802af
Max Kueng <me@maxkueng.com>
2016-11-29 01:18:27 +0100

    Add tests for setX and setY

---
+++ src/setX.spec.js
@@ -0,0 +1,12 @@
+import { assert } from 'chai';
+import setX from './setX';
+
+describe('#setX()', () => {
+  it('should set the X component of the vector', () => {
+    const v1 = { x: 100, y: 50 };
+
+    const res = setX(v1, 42);
+    assert.equal(res.x, 42);
+    assert.equal(res.y, 50);
+  });
+});
---
+++ src/setY.spec.js
@@ -0,0 +1,12 @@
+import { assert } from 'chai';
+import setY from './setY';
+
+describe('#setY()', () => {
+  it('should set the Y component of the vector', () => {
+    const v1 = { x: 100, y: 50 };
+
+    const res = setY(v1, 42);
+    assert.equal(res.x, 100);
+    assert.equal(res.y, 42);
+  });
+});
