68dd8adb4195182962f69564569a5033261ce513
Max Kueng <me@maxkueng.com>
2016-12-02 04:49:20 +0100

    Reorder exports in lib

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,10 @@
 export { default as setX } from './setX';
 export { default as setY } from './setY';
+export { default as add } from './add';
 export { default as addX } from './addX';
 export { default as addY } from './addY';
-export { default as add } from './add';
+export { default as subtract } from './subtract';
 export { default as subtractX } from './subtractX';
 export { default as subtractY } from './subtractY';
-export { default as subtract } from './subtract';
-export { default as distanceSq } from './distanceSq';
 export { default as distance } from './distance';
+export { default as distanceSq } from './distanceSq';
