b37825c607682a1554ec37f03f91e4dee7822e45
Max Kueng <me@maxkueng.com>
2016-12-02 04:29:45 +0100

    Put type import statements after module import statements

--- src/distance.js
+++ src/distance.js
@@ -1,8 +1,8 @@
 /* @flow */
 
-import type { Vector } from './flowTypes';
-
 import distanceSq from './distanceSq';
+
+import type { Vector } from './flowTypes';
 
 export default function distance(a: Vector, b: Vector): number {
   return Math.sqrt(distanceSq(a, b));
